e8f95d96da7054f7d731fec71af6b9c3a8a66d4e
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2021-03-28 14:44:47 -0300

    Adicionando material design :package:

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: <Widget>[
-          Text(
-            'Deliver features faster',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Hello mundo :)',
-            textDirection: TextDirection.ltr,
-          ),
-          Column(
-            children: <Widget>[
-              Text(
-                'oi',
-                textDirection: TextDirection.ltr,
-              ),
-            ],
-          ),
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.contain, // otherwise the logo will be tiny
-              child: const FlutterLogo(),
-            ), //fittedBox
-          ),
-        ], //<Widget>[]
-      ), //Column
-    );
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tranferencias'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
